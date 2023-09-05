90a9d2d15d82b36b0931aef0d29c0d6b16aa7a25
trufnbeti <trungboy80@gmail.com>
2023-09-05 12:00:25 +0700

    +add propeller

---
+++ assets/_Game/Script/Item/Propeller.ts
@@ -0,0 +1,29 @@
+import GameManager from "../Manager/GameManager";
+import Utilities from "../Utilities";
+import Item from "./Item";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Propeller extends Item {
+    @property(cc.Float) private fallSpeed: number= 0.1;
+    private canFalling: boolean = false;
+
+    protected onCollisionEnter(other: cc.Collider, self: cc.Collider): void{
+        // super.onCollisionEnter(other, self);
+        if (other.node.group == 'player booster'){
+            GameManager.Ins.player.equipBooster(this);
+        }
+        
+    }
+
+    public drop(): void{
+               
+    }
+
+    update (dt: number) {
+        if (this.canFalling){
+            this.node.y -= this.fallSpeed * dt;
+        }
+    }
+}
